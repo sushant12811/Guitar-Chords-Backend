fac4e1ad07a941ec2cef87cf023e7e66b8300ceb
Sushant <steve.malistella@gmail.com>
2025-12-02 14:27:36 -0500

    bug fixed

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,11 +1,11 @@
 import cron from 'cron';
-import http from 'http';
+import https from 'https';
 import { ENV } from './env.js';
 
 // A simple cron job that runs every day at midnight
 const job = new cron.CronJob("*/14 * * * *" , function () {
 
-    http
+    https
     .get(ENV.API_URL, (res) => {
         if (res.statusCode === 200) {console.log("Get request sent successfully") }
         else { console.log(`Request failed with status code: ${res.statusCode}`); }
